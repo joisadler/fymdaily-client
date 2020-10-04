f25cbe0f61717780e71846b851007b370e0a3697
Yossi Adler <joisadler@gmail.com>
2020-10-04 21:11:07 +0300

    start working on user settings page

--- src/components/Settings/UserSettingsPage/index.jsx
+++ src/components/Settings/UserSettingsPage/index.jsx
@@ -1,23 +1,60 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Navbar from '../../Navigation/Navbar';
 
 const UserSettingsPage = () => {
   const user = useSelector(state => state.user.loggedInUser);
-  console.log(user)
+  console.log(user);
   const {
     username,
-    weight,
-    bodyWeight,
-    gender,
-    hipCircumference,
-    waistCircumference,
-    neckCircumference,
-    physicalActivityLevel,
-    goal,
   } = user;
+  const currentHeight = user.height;
+  const currentBodyWeight = user.bodyWeight;
+  const currentGender = user.gender;
+  const currentHipCircumference = user.hipCircumference;
+  const currrentWaistCircumference = user.waistCircumference;
+  const currentNeckCircumference = user.neckCircumference;
+  const currentPhysicalActivityLevel = user.physicalActivityLevel;
+  const currentGoal = user.goal;
   const isUserInfoSet = !!user.height;
+  const [height, setHeight] = useState(currentHeight);
+  const [bodyWeight, setBodyWeight] = useState(currentBodyWeight);
+  const [gender, setGender] = useState(currentGender);
+  const [hipCircumference, setHipCircumference] = useState(currentHipCircumference);
+  const [waistCircumference, setWaistCircumference] = useState(currrentWaistCircumference);
+  const [neckCircumference, setNeckCircumference] = useState(currentNeckCircumference);
+  const [physicalActivityLevel, setPhysicalActivityLevel] = useState(currentPhysicalActivityLevel);
+  const [goal, setGoal] = useState(currentGoal);
+
+  const set = (name) => {
+    switch (name) {
+      case 'height':
+        return setHeight;
+      case 'bodyWeight':
+        return setBodyWeight;
+      case 'gender':
+        return setGender;
+      case 'hipCircumference':
+        return setHipCircumference;
+      case 'waistCircumference':
+        return setWaistCircumference;
+      case 'neckCircumference':
+        return setNeckCircumference;
+      case 'physicalActivityLevel':
+        return setPhysicalActivityLevel;
+      case 'goal':
+        return setGoal;
+      default:
+        break;
+    }
+  };
+
+  const handleChange = ({ value, dataset }) => {
+    const { name } = dataset;
+    set(name)(value);
+  };
 
   return (
     <>
@@ -32,6 +69,107 @@ const UserSettingsPage = () => {
               : 'Please enter the following data (this is necessary for the application to work correctly):'}
           </h2>
         </header>
+        <form className="user-settings-form">
+          <div className="user-settings-input-container">
+            <label
+              htmlFor="user-settings-height-input"
+              className="user-settings-input-label"
+            >
+              Height:
+            </label>
+            <input
+              type="number"
+              id="user-settings-height-input"
+              className="user-settings-input"
+              placeholder="Height in cm"
+              value={height}
+              data-name="height"
+              onChange={e => handleChange(e.target)}
+              min="0"
+              max="300"
+              step="any"
+              required
+            />
+          </div>
+          <div className="user-settings-input-container">
+            <label
+              htmlFor="user-settings-weight-input"
+              className="user-settings-input-label"
+            >
+              Weight:
+            </label>
+            <input
+              type="number"
+              id="user-settings-weight-input"
+              className="user-settings-input"
+              placeholder="Weight in kg"
+              value={bodyWeight}
+              data-name="bodyWeight"
+              onChange={e => handleChange(e.target)}
+              min="0"
+              max="1000"
+              step="any"
+              required
+            />
+          </div>
+          <div className="user-settings-input-container">
+            <label
+              htmlFor="user-settings-gender-input"
+              className="user-settings-input-label"
+            >
+              Gender:
+            </label>
+            <select
+              id="user-settings-gender-input"
+              className="user-settings-input"
+              value={gender}
+              data-name="gender"
+              onChange={e => handleChange(e.target)}
+              required
+            >
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </div>
+          <div className="user-settings-input-container">
+            { gender === 'female' && (
+              <>
+                <label
+                  htmlFor="user-settings-hip-circumference-input"
+                  className="user-settings-input-label"
+                >
+                  Hip Circumference:
+                </label>
+                <input
+                  type="number"
+                  id="user-settings-hip-circumference-input"
+                  className="user-settings-input"
+                  placeholder="Hip circumference in cm"
+                  value={hipCircumference}
+                  data-name="hipCircumference"
+                  onChange={e => handleChange(e.target)}
+                  min="0"
+                  max="500"
+                  step="any"
+                  required
+                />
+              </>
+            )}
+          </div>
+          <div className="user-settings-input-container">
+            <input type="text" />
+          </div>
+          <div className="user-settings-input-container">
+            <input type="text" />
+          </div>
+          <div className="user-settings-input-container">
+            <input type="text" />
+          </div>
+          <div className="user-settings-input-container">
+            <input type="text" />
+          </div>
+          <button />
+        </form>
       </main>
       {isUserInfoSet && <Navbar />}
     </>
