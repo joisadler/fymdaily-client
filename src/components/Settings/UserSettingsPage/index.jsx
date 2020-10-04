d1a43030190e6334b63f930269576b25692d2cb9
Yossi Adler <joisadler@gmail.com>
2020-10-04 19:21:35 +0300

    reorganize typography using scss variables

--- src/components/Settings/UserSettingsPage/index.jsx
+++ src/components/Settings/UserSettingsPage/index.jsx
@@ -1,17 +1,39 @@
+/* eslint-disable max-len */
 import React from 'react';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Navbar from '../../Navigation/Navbar';
 
-// eslint-disable-next-line arrow-body-style
 const UserSettingsPage = () => {
-  // const user = useSelector(state => state.user.loggedInUser);
+  const user = useSelector(state => state.user.loggedInUser);
+  console.log(user)
+  const {
+    username,
+    weight,
+    bodyWeight,
+    gender,
+    hipCircumference,
+    waistCircumference,
+    neckCircumference,
+    physicalActivityLevel,
+    goal,
+  } = user;
+  const isUserInfoSet = !!user.height;
 
   return (
     <>
       <main className="page">
-        User Settings Page
+        <header>
+          <h1 className="page-title">
+            { isUserInfoSet ? `Hey, ${username}!` : `Welcome, ${username}!`}
+          </h1>
+          <h2 className="user-settings-page-subtitle">
+            { isUserInfoSet
+              ? 'We advise you to update this data at least once a month (this is important for the correct work with your weight):'
+              : 'Please enter the following data (this is necessary for the application to work correctly):'}
+          </h2>
+        </header>
       </main>
-      <Navbar />
+      {isUserInfoSet && <Navbar />}
     </>
   );
 };
